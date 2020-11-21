8e3b7c75ed348b84e1d946b9a32da96a5aef0501
zhilu2000 <sgh2759@closeli.cn>
2020-11-21 11:19:25 +0800

    median

--- src/main/java/leetcode/editor/en/MedianOfTwoSortedArrays.java
+++ src/main/java/leetcode/editor/en/MedianOfTwoSortedArrays.java
@@ -60,12 +60,89 @@ package leetcode.editor.cn;
 public class MedianOfTwoSortedArrays{
 	public static void main(String[] args) {
 		Solution solution = new MedianOfTwoSortedArrays().new Solution();
-		
+		int a [] ={1,2};
+		int b [] ={3,4};
+        System.out.println(solution.findMedianSortedArrays(a,b));
+
 	}
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return 0;
+        return userNewArray(nums1,nums2);
+
+    }
+
+    /**
+     * 空间换时间
+     */
+    private double userNewArray(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        int[] arr = null;
+        if (len == nums1.length) {
+            arr = nums1;
+        } else if (len == nums2.length) {
+            arr = nums2;
+        } else {
+            arr = new int[len];
+            for (int i = 0, j = 0, k = 0; i < nums1.length || j < nums2.length; ) {
+                if (i == nums1.length) {
+                    arr[k++] = nums2[j++];
+                } else if (j == nums2.length) {
+                    arr[k++] = nums1[i++];
+                } else {
+                    if (nums1[i] > nums2[j]) {
+                        arr[k++] = nums2[j++];
+                    } else {
+                        arr[k++] = nums1[i++];
+                    }
+                }
+
+                if (k > len / 2) {
+                    break;
+                }
+            }
+        }
+        if (len % 2 == 0) {
+            return (double) (arr[len / 2 - 1] + arr[len / 2]) / 2;
+        } else {
+            return arr[len / 2];
+        }
+    }
+
+    private double standard(int[] A, int[] B){
+        int m = A.length;
+        int n = B.length;
+        if (m > n) { // to ensure m<=n
+            int[] temp = A; A = B; B = temp;
+            int tmp = m; m = n; n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j-1] > A[i]){
+                iMin = i + 1; // i is too small
+            }
+            else if (i > iMin && A[i-1] > B[j]) {
+                iMax = i - 1; // i is too big
+            }
+            else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
